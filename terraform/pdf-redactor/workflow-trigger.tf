90a57152e9a70222650d9b41c91292611961df30
Pipe Martina <fmartina@google.com>
2022-07-19 18:06:41 +0000

    Updates to permissions for event-arc trigger

--- terraform/pdf-redactor/workflow-trigger.tf
+++ terraform/pdf-redactor/workflow-trigger.tf
@@ -48,6 +48,28 @@ resource "google_project_iam_member" "workflow_trigger" {
   member  = "serviceAccount:${google_service_account.workflow_trigger.email}"
 }
 
+# GCP creates a special SA for GCS that needs to be granted pub/sub permissions
+# ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/storage_project_service_account
+data "google_project" "project" {
+}
+data "google_storage_project_service_account" "gcs_account" {
+}
+locals {
+    pubsub_default_sa_email = "service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "gcs_sa_to_pubsub" {
+  project = var.project_id
+  role    = "roles/pubsub.publisher"
+  member  = "serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"
+}
+
+resource "google_project_iam_member" "pubsub_sa_token_creator" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = "serviceAccount:${local.pubsub_default_sa_email}"
+}
+
 # resource "google_cloudfunctions_function" "function" {
 #   name = "workflow-trigger${local.app_suffix}"
 
@@ -76,7 +98,6 @@ resource "google_project_iam_member" "workflow_trigger" {
 # }
 
 resource "google_eventarc_trigger" "primary" {
-  provider        = google-beta
   name            = "workflow-trigger${local.app_suffix}"
   location        = var.region
   service_account = google_service_account.workflow_trigger.email
@@ -95,5 +116,7 @@ resource "google_eventarc_trigger" "primary" {
 
   depends_on = [
     module.project_services,
+    google_project_iam_member.gcs_sa_to_pubsub,
+    google_project_iam_member.pubsub_sa_token_creator,
   ]
 }
